e258be530588bf25cfb6ef827c6d71aed49c8d51
ThonyMarckDEV <thonymarck385213xd@gmail.com>
2026-01-20 19:58:15 -0500

    Corrected

--- src/js/authToken.js
+++ src/js/authToken.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import API_BASE_URL from './urlHelper';
+import API_BASE_URL from 'js/urlHelper';
 import jwtUtils from 'utilities/Token/jwtUtils';
 
 let refreshPromise = null;
